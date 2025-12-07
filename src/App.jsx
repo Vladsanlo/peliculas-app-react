02720919c7840da05ab7aa9b96c3568f109877d1
Vladimir Sanchez <vladypuggu@gmail.com>
2025-12-06 19:40:10 -0600

    Created App.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './header'
+import Scifi from './sci-fi-component'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(Scifi)
+
+  const handleChangePage = event =>
+    setPage(event.target.id)
 
   return (
     <>
-    
-
+      <Header callback={handleChangePage} />
+      <h1>{page}</h1>
+      {page === 'sci-fi' ? <sci-fi-component /> : ''}
+      
     </>
-  )
+  );
 }
 
 export default App
