48d2713e2e9648d920f20144ae1ae8224b631653
Vladimir Sanchez <vladypuggu@gmail.com>
2025-12-06 18:52:55 -0600

    initial commit

---
+++ src/header.jsx
@@ -0,0 +1,17 @@
+function Header({callback}){
+    return(
+        <header>
+            <h1>Peliculas App </h1>
+            <nav>
+                <ul>
+                    <li id="sci-fi" onClick={callback}>Sci-fi</li>
+                    <li id="terror" onClick={callback}>Terror</li>
+                    <li id="thriller" onClick={callback}>Thrillers</li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header 
